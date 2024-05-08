e97a384225bd482d96987ee8251bfae756322cfa
Adrian <apparenelle@gmail.com>
2024-05-08 18:07:11 -0400

    base
    created foundation for web app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import pink_flask from './assets/pink-flask.jpg';
+import logo from './assets/gym_nut_blue.png'
+import vid from './assets/exercise_vid.mp4'
 
 function App() {
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       
-      <body>
+      {/* Nav header that overlays and transparent */}
+      <div className='header flex-row align-space-around'>
+        <button className='button'>About</button>
+        <img src={logo} className="logo" />
+        <button className='button'>Bag</button>
+      </div>
 
-      </body>
+      <div class="video-background">
+        <video autoPlay loop muted playsInline>
+            <source src={vid} type="video/mp4" />
+        </video>
+        <div class="content">
+            {/* <h1>Welcome to My Website</h1>
+            <p>Here is some introductory text.</p> */}
+        </div>
+      </div>
+
+      
+      {/* main page body */}
+      <div className='products-page flex-column'>
+        <h4>Wellness Product Name</h4>
+        <img src={pink_flask} alt="pink water container"/>
+        
+        <>Name of Product</>
+        <p>Description of the product. How it works and functions</p>
+
+      </div>
     </div>
   );
 }
