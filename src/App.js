2477704de7ccb765bced44903580207f8ef8fcfd
Adrian <apparenelle@gmail.com>
2024-05-23 19:51:37 -0400

    tweaks

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ import logo from './assets/gymhut2.png'
 import vid from './assets/exercise_vid.mp4'
 import ShopItem from './ShopItem.js';
 
-
 function App() {
-  
   return (
-    <div className="App">
-      
+    <div className="App flex-column">
       {/* Nav header that overlays and transparent */}
       <div className='header flex-row jcontent-space-around aitems-center'>
         <button className='button'>About</button>
@@ -24,26 +21,39 @@ function App() {
             <source src={vid} type="video/mp4" />
         </video>
         <div className="content">
-            {/* <h1>Welcome to My Website</h1>
-            <p>Here is some introductory text.</p> */}
+            <h1>Assessments Available</h1>
+            <div className='flex-row jcontent-space-around'>
+              <button className='button'>Need a trainer</button>
+              <button className='button'>Want some gear</button>
+            </div>
+            {/* <p>Here is some introductory text.</p> */}
         </div>
       </div>
       
-      <div className='flex-row jcontent-space-around aitems-center panel1'>
+      <div 
+        className='panel1 flex-row jcontent-space-around aitems-center'
+      >
         <p>Just Do It.</p>
         <p>Find the new you.</p>
         <p>SHOP ALL</p>
       </div>
       
-      <div className='flex-row'>
-        <ShopItem key={1}/>
-        <ShopItem key={2}/>
-        
-        <ShopItem key={3}/>
-        <ShopItem key={4}/>
+      <div 
+        className='flex-column'
+      >
+        <div className='flex-row'>
+          <ShopItem key={1}/>
+          <ShopItem key={2}/> 
+        </div>
+        <div className='flex-row'>
+          <ShopItem key={3}/>
+          <ShopItem key={4}/>
+        </div>
       </div>
      
-      <div className='flex-row jcontent-space-around aitems-center panel1'>
+      <div 
+        className='panel1 flex-row jcontent-space-around aitems-center'
+      >
         <p>Just Do It.</p>
         <p>Find the new you.</p>
         <p>SHOP ALL</p>
@@ -54,7 +64,7 @@ function App() {
         <h4>Wellness Product Name</h4>
         <img src={pink_flask} alt="pink water container"/>
         
-        <>Name of Product</>
+        <p>Name of Product</p>
         <p>Description of the product. How it works and functions</p>
 
       </div>
--- src/ShopItem.js
+++ src/ShopItem.js
@@ -3,7 +3,7 @@ import pink_flask from './assets/pink_flask.png';
 import action_shot from './assets/action_shot.jpg';
 import './ShopItem.css';
 
-export default function ShopItem() {
+export default function ShopItem(props) {
     const name = "Hydroflask";
     const cost = 44;
     const colors = ["pink", "green", "blue"];
@@ -22,40 +22,149 @@ export default function ShopItem() {
     };
 
     return (
-        <div 
-            className="shop-item flex-row jcontent-center flex-grow-1 item-wrapper" 
+        <div
+            className={`shop-item flex-row jcontent-center flex-grow-1  ${isHovered ? 'hovered' : ''}`}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
+            key={props.key}
         >
-            
-            <div className="item-info">
-                <div className="flex-row jcontent-space-between">
-                    <p>{name}</p>
-                    <p>{colors.length} Colors</p>
-                </div>
-                <div className="flex-row jcontent-space-between aitems-center">
-                    <p>${cost}</p>
-                    <div className="flex-row">
-                        {colors.map((color, index) => (
-                            <div
-                            key={index}
-                            className="variant-colors"
-                            style={{ backgroundColor: color }}
-                            ></div>
-                        ))}
+            {!isHovered ? (
+                <div className="item-info item-wrapper flex-column">
+                    <div className="flex-row jcontent-space-between">
+                        <p>{name}</p>
+                        <p>{colors.length} Colors</p>
                     </div>
+                    <div className="flex-row jcontent-space-between aitems-center">
+                        <p>${cost}</p>
+                        <div className="flex-row">
+                            {colors.map((color, index) => (
+                                <div
+                                    key={index}
+                                    className="variant-colors"
+                                    style={{ backgroundColor: color }}
+                                ></div>
+                            ))}
+                        </div>
+                    </div>
+                    <img 
+                        src={imageDefault} 
+                        alt={name} 
+                        className="item-image"
+                    />
                 </div>
-            <img 
-                // src={isHovered ? imageInUse : imageDefault} 
-                src={imageDefault}
-                alt={name} 
-                className="item-image"
-            />
-
-            </div>
-            {isHovered && (
-                <button className="add-to-cart" onClick={() => console.log(`${name} added to cart`)}>Add to Cart</button>
+            ) : (
+                <div className='flex-column flex-grow-1'>
+                    <img 
+                        src={imageInUse} 
+                        alt={name} 
+                        className="item-image-hover flex-grow-9 flex-shrink-1"
+                    />
+                    <button 
+                        className="add-to-cart flex-grow-1 flex-shrink-1" 
+                        onClick={() => console.log(`${name} added to cart`)}
+                        // aria-label={`Add ${name} to cart`}
+                    >
+                        QUICK ADD
+                    </button>
+                </div>
             )}
         </div>
     );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import pink_flask from './assets/pink_flask.png';
+// import action_shot from './assets/action_shot.jpg';
+// import './ShopItem.css';
+
+// export default function ShopItem(props) {
+//     const name = "Hydroflask";
+//     const cost = 44;
+//     const colors = ["pink", "green", "blue"];
+//     const imageDefault = pink_flask;
+//     const imageInUse = action_shot;
+//     const stats = ["800mL / 16oz", "bacteria resistant"];
+
+//     const [isHovered, setIsHovered] = useState(false);
+
+//     const handleMouseEnter = () => {
+//         setIsHovered(true);
+//     };
+
+//     const handleMouseLeave = () => {
+//         setIsHovered(false);
+//     };
+    
+//     if (!isHovered){
+//         return (
+//             <div 
+//                 className="shop-item flex-row jcontent-center flex-grow-1 item-wrapper" 
+//                 onMouseEnter={handleMouseEnter}
+//                 onMouseLeave={handleMouseLeave}
+//                 key={props.key}
+
+//             >
+                
+//                 <div className="item-info flex-column">
+//                     <div className="flex-row jcontent-space-between">
+//                         <p>{name}</p>
+//                         <p>{colors.length} Colors</p>
+//                     </div>
+//                     <div className="flex-row jcontent-space-between aitems-center">
+//                         <p>${cost}</p>
+//                         <div className="flex-row">
+//                             {colors.map((color, index) => (
+//                                 <div
+//                                 key={index}
+//                                 className="variant-colors"
+//                                 style={{ backgroundColor: color }}
+//                                 ></div>
+//                             ))}
+//                         </div>
+//                     </div>
+//                 <img 
+//                     src={imageDefault} 
+//                     // src={imageDefault}
+//                     alt={name} 
+//                     className="item-image"
+//                 />
+    
+//                 </div>                
+//             </div>
+//         );
+//     } else {
+//         return(
+            
+//             <div className='flex-column'
+//                 onMouseEnter={handleMouseEnter}
+//                 onMouseLeave={handleMouseLeave}
+//                 key={props.key}
+//             >
+//                 <img 
+//                     src={imageInUse} 
+//                     // src={imageDefault}
+//                     alt={name} 
+//                     className="item-image"
+//                 />
+                    
+//                 <button className="add-to-cart" 
+//                         onClick={() => console.log(`${name} added to cart`)}
+//                 >Add to Cart</button>
+
+//             </div>
+                
+//         );
+//     }
+    
+// }
